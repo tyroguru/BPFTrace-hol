9e3e9f3283f46b869e66318af38b6c5e0712f82e
Jon Haslam <jonhaslam@meta.com>
2025-01-28 09:23:48 -0800

    extra kprobe related workload

--- CODE/kprobes/kprobeme.cc
+++ CODE/kprobes/kprobeme.cc
@@ -3,19 +3,43 @@
 #include <sys/syscall.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <limits.h>
+#include <mutex>
+#include <syncstream>
 
 #include <iostream>
 #include <string>
 #include <thread>
 
 using namespace std::chrono_literals;
+std::mutex testMutex;
+
+void blockers(void) {
+  /*
+   * Create some contention for the static tracepoint exercise
+   * although this may not actually do what I want. Leave it here for
+   * now anyway.
+   */
+  auto f = [](void) {
+    const std::lock_guard<std::mutex> lock(testMutex);
+    std::this_thread::sleep_for(1s);
+  };
+
+  std::thread t(f);
+  std::thread t1(f);
+  std::thread t2(f);
+
+  t.join();
+  t1.join();
+  t2.join();
+}
 
 int main() {
   int err;
   int fd = -1;
   char buf[1024];
 
-  for (int i = 0; i < 120; ++i) {
+  for (auto i = 0; i < INT_MAX; ++i) {
     fd = ::syscall(SYS_open, "/proc/uptime", O_RDONLY);
     if (fd < 0) {
       ::perror("open");
@@ -40,6 +64,8 @@ int main() {
       close(fd);
       unlink(filename);
     }
+
+    blockers();
 
     std::this_thread::sleep_for(1s);
   }
